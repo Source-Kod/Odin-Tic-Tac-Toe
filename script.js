c47ae874f12bf1f12dbbe03a5a24350ff0653137
Source-Kod <Source-Kod@protonmail.com>
2023-05-03 20:30:02 -0500

    add selecting cell and improved player factory

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const gameBoard = (() => {
-  const array = ['X', 'X', 'O', 'O', 'X', 'O', 'X', 'O', 'X'];
+  const array = [
+    'empty',
+    'empty',
+    'empty',
+    'empty',
+    'empty',
+    'empty',
+    'empty',
+    'empty',
+    'empty',
+  ];
   const gameboardContainer = document.querySelector('#gameboard-container');
 
+  const deleteBoard = () => {
+    while (gameboardContainer.firstChild) {
+      gameboardContainer.removeChild(gameboardContainer.firstChild);
+    }
+  };
+
   const drawBoard = () => {
-    array.forEach((e) => {
+    array.forEach((e, index) => {
       const gridCell = document.createElement('div');
 
       if (e === 'X') {
@@ -18,21 +34,51 @@ const gameBoard = (() => {
         gridCell.appendChild(gridCellImg);
       }
 
+      if (e === 'empty') {
+        gridCell.addEventListener('click', () => {
+          if (game.checkWhichPlayerTurn() === 1) {
+            player1.selectMove(index);
+          } else {
+            player2.selectMove(index);
+          }
+          game.changePlayerTurn();
+          deleteBoard();
+          drawBoard();
+        });
+      }
+
       gameboardContainer.appendChild(gridCell);
+
+      gridCell.classList = 'w-44 h-44 border border-solid';
     });
   };
-  return { drawBoard };
+
+  const changeArray = (index, playerLetter) => {
+    array[index] = playerLetter;
+  };
+  return { drawBoard, changeArray, deleteBoard };
 })();
 
-const playerFactory = (name) => {
-  const sayHello = () => console.log(`hello ${name}`);
-  return { sayHello };
+const game = (() => {
+  let turn = 1;
+
+  const checkWhichPlayerTurn = () => turn;
+
+  const changePlayerTurn = () => {
+    turn = turn === 1 ? 2 : 1;
+  };
+
+  return { checkWhichPlayerTurn, changePlayerTurn };
+})();
+
+const playerFactory = (playerLetter) => {
+  const selectMove = (index) => {
+    gameBoard.changeArray(index, playerLetter);
+  };
+  return { selectMove };
 };
 
-const player1 = playerFactory('player1');
-const player2 = playerFactory('player2');
-
-player1.sayHello();
-player2.sayHello();
+const player1 = playerFactory('X');
+const player2 = playerFactory('O');
 
 gameBoard.drawBoard();
